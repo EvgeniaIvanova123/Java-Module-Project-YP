3cf579c9231cbbeb8af18a04818b68ae8d6d8199
evgenia <Zolotayledy@yandex.ru>
2026-02-23 12:50:03 +0300

    исправлены комментарии

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,24 @@ public class Main {
         for (int i = 0; i <= 2; i++) {
             System.out.println("Введите название автомобиля");
             String carName = scanner.nextLine();
+            while
+            (carName.isBlank() || !carName.matches("[а-яА-Яa-zA-Z]+")) {
+                System.out.println("Название автомобиля некорректно, задайте название заново");
+                carName = scanner.nextLine();
+            }
 
             System.out.println("Введите скорость автомобиля " + carName);
-            int speedCar = scanner.nextInt();
-            scanner.nextLine();
+            String speedInput = scanner.nextLine();
+
+            while (!speedInput.matches("[0-9]+")) {
+                System.out.println("Ошибка: введите целое число");
+                speedInput = scanner.nextLine();
+            }
+            int speedCar = Integer.parseInt(speedInput);
+
             while (speedCar < 0 || speedCar > 250) {
-                System.out.println("Значение скорости " + speedCar + " некооректно, задайте скорость заново");
+                System.out.println("Значение скорости " + speedCar + " некорректно. Введите целое число от 0 до 250");
                 speedCar = scanner.nextInt();
-                scanner.nextLine();
             }
             Car car = new Car(carName, speedCar);
             race.addCar(car);
@@ -28,6 +38,7 @@ public class Main {
 
 class Race {
     ArrayList<Car> cars = new ArrayList<>();
+
     void addCar(Car car) {
         cars.add(car);
     }
